8b015085fc76accf17c5be6ce52fa492cf515be6
EmetMet <antykoma@yandex.ru>
2022-03-30 11:46:56 +0300

    Подформатировал code style

--- src/main/java/print_ln.java
+++
@@ -1,8 +0,0 @@
-public class print_ln {
-
-    public static void main(String[] args) {
-
-        System.out.println("Java sucks!");
-    }
-
-}
---
+++ src/main/java/ru/pack/java/testing/PrintLn.java
@@ -0,0 +1,10 @@
+package ru.pack.java.testing;
+
+public class PrintLn
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Java sucks!");
+    }
+
+}
