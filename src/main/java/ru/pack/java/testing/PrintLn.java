3dc0e3502d7c107a86cb7b7a5d380c80926e64ce
EmetMet <antykoma@yandex.ru>
2022-03-31 18:09:40 +0300

    Докинул пару функций

--- src/main/java/ru/pack/java/testing/PrintLn.java
+++ src/main/java/ru/pack/java/testing/PrintLn.java
@@ -4,7 +4,31 @@ public class PrintLn
 {
     public static void main(String[] args)
     {
-        System.out.println("Java sucks!");
+        LanguageSucks("Java");
+        LanguageSucks("Kotlin");
+        LanguageSucks("Python");
+        LanguageSucks("C");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со стороной " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void LanguageSucks(String language)
+    {
+        System.out.println(language + " sucks" + "!");
+    }
+
+    public static double area(double len)
+    {
+        return len * len;
+    }
+
+    public static double area(double a, double b)
+    {
+        return a * b;
     }
 
 }
