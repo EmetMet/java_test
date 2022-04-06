27fda629478ddd773f01821c7469de873bb6a498
EmetMet <antykoma@yandex.ru>
2022-04-06 17:45:41 +0300

    Результат 2.9

--- src/main/java/ru/pack/java/testing/PrintLn.java
+++ src/main/java/ru/pack/java/testing/PrintLn.java
@@ -9,11 +9,14 @@ public class PrintLn
         LanguageSucks("Python");
         LanguageSucks("C");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со стороной " + a + " и " + b + " = " + area(a, b));
+        Square s = new Square(5);
+        Rectangle r = new Rectangle(4, 6);
+//        s.l = 5;
+//        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+//         r.a = 4;
+//         r.b = 6;
+        System.out.println("Площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void LanguageSucks(String language)
@@ -21,14 +24,14 @@ public class PrintLn
         System.out.println(language + " sucks" + "!");
     }
 
-    public static double area(double len)
-    {
-        return len * len;
-    }
+//    public static double area(Square s)
+//    {
+//        return s.l * s.l;
+//    }
 
-    public static double area(double a, double b)
-    {
-        return a * b;
-    }
+//    public static double area(Rectangle r)
+//    {
+//        return r.a * r.b;
+//    }
 
 }
---
+++ src/main/java/ru/pack/java/testing/Square.java
@@ -0,0 +1,17 @@
+package ru.pack.java.testing;
+
+public class Square
+{
+    public double l;
+
+    public Square(double l)
+
+    {
+        this.l = l;
+    }
+
+    public double area()
+    {
+        return this.l * this.l;
+    }
+}
